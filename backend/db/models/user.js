82073ec106ae69d4b211a788fb5762fff2c590eb
cstandre <caitlynstandre@gmail.com>
2023-01-12 20:43:09 -0800

    Added User authentication Express middleware

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
       const { id, username, email } = this;
-      return { id, username, email};
+      console.log(id, username, email)
+      return { id, username, email };
     }
 
     validatePassword(password) {
@@ -32,67 +33,67 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
 
-    static async signup ({ username, email, password }) {
+    static async signup({ username, email, password }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
         email,
         hashedPassword
       });
-      return await User.scope('currentUser').findByPk(user.id)
+      return await User.scope('currentUser').findByPk(user.id);
     }
 
     static associate(models) {
       // define association here
     }
   }
-  User.init({
-    username: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [4,30],
-        isNotEmail(value) {
-          if (validator.isEmail(value)) {
-            throw new Error("Cannot be an email");
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
           }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
         }
       }
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [3,256],
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [60,60]
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] }
+        },
+        loginUser: {
+          attributes: {}
+        }
       }
     }
-  }, {
-    defaultScope: {
-      attributes:{
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
-      }
-    },
-    scopes: {
-      currentUser: {
-        attributes: { exclude: ['hashedPassword'] }
-      },
-      loginUser: {
-        attributes: {}
-      }
-    },
-
-    sequelize,
-    modelName: 'User',
-  });
+  );
   return User;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth')
+const { User } = require('../../db/models');
+
+router.use(restoreUser)
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body })
-})
+});
+
+
+            // TESTING AUTH MIDDLEWARES //
+// const { setTokenCookie } = require('../../utils/auth');
+// router.get('/set-token-cookie', async(_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'CaitlynStAndre'
+//         }
+//     });
+//     console.log(user)
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
+
+// router.get('/restore-user',(req, res) => {
+//       return res.json(req.user);
+//     }
+//   );
+
 
 module.exports = router;
