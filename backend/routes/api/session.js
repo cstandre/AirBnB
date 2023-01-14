117e687e4b938b267eddd7b8ae909bc83d39a253
cstandre <caitlynstandre@gmail.com>
2023-01-14 11:53:27 -0800

    Added a backend endpoint to ge the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,7 +32,14 @@ router.post(
     return res.json({ message: 'Success' });
   });
 
-  
+  router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    } else return res.json();
+  });
 
 
 
