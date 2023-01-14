0c73b156f1c0c74a3b143995809aa7a0ed609d92
cstandre <caitlynstandre@gmail.com>
2023-01-14 10:58:01 -0800

    Added User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const userRouter = require('./users')
 const { restoreUser } = require('../../utils/auth')
-const { User } = require('../../db/models');
 
-router.use(restoreUser)
+router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', userRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body })
