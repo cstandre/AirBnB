f4c815eb3e888d6e9192469555ee56eb802be99c
cstandre <caitlynstandre@gmail.com>
2023-02-22 09:52:41 -0800

    redux setup

---
+++ frontend/src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+});
+
+let enhancer;
+
+// sets different store enhancers depending on if the Node environment is in development or production.
+
+if (process.env.NODE_ENV === 'production') {
+    // production, enhancer only applies thunk middleware
+    enhancer = applyMiddleware(thunk)
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+    // uses redux tools
+    // uses thunk and logger middleware
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+};
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
